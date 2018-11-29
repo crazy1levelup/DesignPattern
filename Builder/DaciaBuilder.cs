5d62038ef7b00b20830eb96a7f022a5cc6b95862
crazy1levelup <trif.eduard@yahoo.ro>
2018-11-29 17:47:14 +0200

    Added DaciaBuilder

---
+++ Builder/DaciaBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class DaciaBuilder : IVehicleBuilder
+    {
+
+        Vehicle objBuilder = new Vehicle();
+
+        public void SetAccessories()
+        {
+            objBuilder.Accessories.Add("MaybeBreaks");
+            objBuilder.Accessories.Add("MaybeAirBags");
+        }
+
+        public void SetBody()
+        {
+            objBuilder.Body = "Metal";
+        }
+
+        public void SetEngine()
+        {
+            objBuilder.Engine = "Existent";
+        }
+
+        public void SetModel()
+        {
+            objBuilder.Model = "1310";
+        }
+
+        public void SetTransmission()
+        {
+            objBuilder.Transmission = "30km/h";
+        }
+
+        public Vehicle GetVehicle()
+        {
+            return objBuilder;
+        }
+    }
+}
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -10,17 +10,24 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            var vehicleCreator = new VehicleCreator(new HeroBuilder());
-            vehicleCreator.CreateVehicle();
-            var vehicle = vehicleCreator.GetVehicle();
-            vehicle.ShowInfo();
+            var vehicleCreator1 = new VehicleCreator(new HeroBuilder());
+            vehicleCreator1.CreateVehicle();
+            var vehicle1 = vehicleCreator1.GetVehicle();
+            vehicle1.ShowInfo();
 
             Console.WriteLine("--------------------------------------");
 
-            vehicleCreator = new VehicleCreator(new HondaBuilder());
-            vehicleCreator.CreateVehicle();
-            vehicle = vehicleCreator.GetVehicle();
-            vehicle.ShowInfo();
+            var vehicleCreator2 = new VehicleCreator(new HondaBuilder());
+            vehicleCreator2.CreateVehicle();
+            var vehicle2 = vehicleCreator2.GetVehicle();
+            vehicle2.ShowInfo();
+
+            Console.WriteLine("--------------------------------------");
+
+            var vehicleCreator3 = new VehicleCreator(new DaciaBuilder());
+            vehicleCreator3.CreateVehicle();
+            var vehicle3 = vehicleCreator3.GetVehicle();
+            vehicle3.ShowInfo();
 
             Console.ReadLine();
         }
