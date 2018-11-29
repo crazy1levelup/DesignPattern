f91cf683ed1e1984c68fc447800c8dd53abf60be
crazy1levelup <trif.eduard@yahoo.ro>
2018-11-29 17:35:15 +0200

    Add project files.

---
+++ Builder/HeroBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class HeroBuilder : IVehicleBuilder
+    {
+        Vehicle objVehicle = new Vehicle();
+
+       
+
+        public void SetAccessories()
+        {
+            objVehicle.Accessories.Add("Seat Cover");
+            objVehicle.Accessories.Add("Rear Mirror");
+           
+        }
+
+        public void SetBody()
+        {
+            objVehicle.Body = "Plastic";
+        }
+
+        public void SetEngine()
+        {
+            objVehicle.Engine = "4 Stroke";
+        }
+
+        public void SetModel()
+        {
+            objVehicle.Model = "Hero";
+        }
+
+        public void SetTransmission()
+        {
+            objVehicle.Transmission = "120km/h"; 
+        }
+        public Vehicle GetVehicle()
+        {
+            return objVehicle;
+        }
+    }
+}
---
+++ Builder/HondaBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class HondaBuilder : IVehicleBuilder
+    {
+        Vehicle objVehicle = new Vehicle();
+
+        public void SetAccessories()
+        {
+            objVehicle.Accessories.Add("Seat Cover");
+            objVehicle.Accessories.Add("Rear Mirror");
+            objVehicle.Accessories.Add("Helmet");
+        }
+
+        public void SetBody()
+        {
+            objVehicle.Body = "Plastic";
+        }
+
+        public void SetEngine()
+        {
+            objVehicle.Engine = "4 Stroke";
+        }
+
+        public void SetModel()
+        {
+            objVehicle.Model = "Honda";
+        }
+
+        public void SetTransmission()
+        {
+            objVehicle.Transmission = "125km/h";
+        }
+
+        public Vehicle GetVehicle()
+        {
+            return objVehicle;
+        }
+    }
+}
